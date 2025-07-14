642578b9da5d8027b050f99991f83c2bef686063
서한유 <hu8232@naver.com>
2025-07-15 00:37:37 +0900

    광고ID 권한추가

--- lib/admob/AdMobService.dart
+++ lib/admob/AdMobService.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
@@ -9,7 +9,9 @@ class AdMobService {
   //전면 광고
   static String? get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return kReleaseMode
+          ? 'ca-app-pub-3443570983692474/1967011102' // 릴리즈용
+          : 'ca-app-pub-3940256099942544/1033173712'; // 디버그용 (테스트 ID)
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/4411468910';
     }
