f7f8150938c94770a57e4d1d402141399cd51767
서한유 <hu8232@naver.com>
2025-07-17 10:05:55 +0900

    info.plist , podfile 수정 아직해결X

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:follow_video/views/camera_pose_detection.dart';
 import 'package:follow_video/views/mene_page.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:media_store_plus/media_store_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:io' show Platform;
+// import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
 Future<void> requestCameraPermission() async {
   var status = await Permission.camera.status;
@@ -13,16 +14,34 @@ Future<void> requestCameraPermission() async {
 }
 
 Future<void> _requestPermissions() async {
-  await Permission.storage.request();
+  if (Platform.isAndroid) {
+    await Permission.storage.request();
+  } else if (Platform.isIOS) {
+    await Permission.photos.request(); // 또는 mediaLibrary 등
+  }
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // 광고 초기화
   MobileAds.instance.initialize();
+
+  // iOS에서만 광고 추적 권한 요청
+  // if (Platform.isIOS) {
+  //   final status = await AppTrackingTransparency.trackingAuthorizationStatus;
+  //   if (status == TrackingStatus.notDetermined) {
+  //     await AppTrackingTransparency.requestTrackingAuthorization();
+  //   }
+  // }
+
   await requestCameraPermission(); // 실행 시 권한 요청
   await _requestPermissions(); // 실행 시 권한 요청
-  await MediaStore.ensureInitialized();
-  MediaStore.appFolder = 'MyPoseVideos';
+
+  // MediaStore 초기화 (안드로이드 전용)
+  if (Platform.isAndroid) {
+    await MediaStore.ensureInitialized();
+    MediaStore.appFolder = 'MyPoseVideos';
+  }
   runApp(const MyApp());
 }
 
